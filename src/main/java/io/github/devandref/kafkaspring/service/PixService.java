264fa90953a6e4617505ebd5bcd871f57e4fcde6
devandref <yoandredepaula@gmail.com>
2024-01-28 13:38:23 -0300

    include: kafka-producer

--- src/main/java/io/github/devandref/kafkaspring/service/PixService.java
+++ src/main/java/io/github/devandref/kafkaspring/service/PixService.java
@@ -6,6 +6,7 @@ import io.github.devandref.kafkaspring.dto.PixDTO;
 import io.github.devandref.kafkaspring.model.Pix;
 import io.github.devandref.kafkaspring.repository.PixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,9 @@ public class PixService {
 
     @Autowired
     private BuscaKeyComponent buscaKeyComponent;
+
+    @Autowired
+    private KafkaTemplate<String, Pix> kafkaTemplate;
 
     public PixDTO salvarPix(PixDTO pixDTO) {
         Boolean statusChaveOrigem  = buscaKeyComponent.verificaSeChaveExiste(pixDTO.getChaveOrigem());
@@ -28,7 +32,7 @@ public class PixService {
             pixEntity.setStatus(PixStatus.ERRO);
         }
         pixRepository.save(pixEntity);
-
+        kafkaTemplate.send("pix-topic", pixEntity.getIdentifier(), pixEntity);
         pixDTO.setPixStatus(pixEntity.getStatus());
         return pixDTO;
     }
